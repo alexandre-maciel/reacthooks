ec30cd47dad3e1aca9ec5c9744a671e7ddf28368
Alexandre Maciel <60267287+alexandre-maciel@users.noreply.github.com>
2022-03-15 15:28:03 -0300

    useRef

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,94 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+function ImparPar(num) {
+  const n1 = parseInt(num);
+  if (n1 % 2 === 0) return "Par";
+  if (n1 % 2 === 1) return "Ímpar";
+}
+
+const UseEffect = (props) => {
+  //? ================= Exercicio 01 ==================
+
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      if (fatorial > 1000000) {
+        document.title = "Eita!!!";
+      }
+    },
+    [fatorial]
+  );
+
+  //? ================= Exercicio 02 ==================
+
+  const [number1, setNumber1] = useState(1);
+  const [ParImpar, setParImpar] = useState("");
+
+  useEffect(
+    function () {
+      setParImpar(ImparPar(number1));
+    },
+    [number1]
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+
+      <SectionTitle title="Exercicio 01" />
+
+      <div className="center">
+        <div>
+          <span className="text">Fatorial:</span>
+          <span className="text red">
+            {fatorial === -1 ? "Não existe" : fatorial}
+          </span>
+        </div>
+        <input
+          className="input"
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercicio 02" />
+
+      <div className="center">
+        <div>
+          <span className="text">O número é: </span>
+          <span className="text red">{ParImpar}</span>
+        </div>
+        <input
+          className="input"
+          type="number"
+          value={number1}
+          onChange={(e) => setNumber1(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
